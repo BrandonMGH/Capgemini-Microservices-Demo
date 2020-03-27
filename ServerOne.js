1a31870a06bb737d0be12bfa056f6ea85621b6c2
BrandonMGH <BrandonMGH@Yahoo.com>
2020-03-26 19:16:00 -0700

    added images to server objects

--- ServerOne.js
+++ ServerOne.js
@@ -9,43 +9,50 @@ let heroes = [
     {
         id: 0,
         name: 'Mr. Incredible',
-        superPower: 'Super Speed'
+        superPower: 'Super Speed',
+        img: 'https://vignette.wikia.nocookie.net/disney/images/d/d2/Profile_-_Bob_Parr.jpeg/revision/latest?cb=20190313155821'
     },
     {
         id: 1,
         name: 'Elasti-Girl',
-        superPower: 'Can stretch her body into any shape'
+        superPower: 'Body-morphing',
+        img: 'https://upload.wikimedia.org/wikipedia/en/e/ef/Helen_Parr.png'
     },
     {
         id: 2,
         name: 'Dash',
-        superPower: 'Super Speed'
+        superPower: 'Super Speed',
+        img: 'https://upload.wikimedia.org/wikipedia/en/2/25/Image_of_Dashiell_%22Dash%22_Par_as_he_appears_in_The_Incredibles_and_it%27s_sequel.png'
     },
     {
         id: 3,
         name: 'Violet',
-        superPower: 'Invisibility and Force Fields'
+        superPower: 'Invisibility and Force Field Projection',
+        img: 'https://vignette.wikia.nocookie.net/the-incredibles/images/3/36/I2_-_Violet_2.png/revision/latest?cb=20181206194936'
     },
 ];
 
-app.get('/', (req, res)=>{
-    res.send('Server is running');
+app.get('/', (req, res) => {
+    res.send('Welcome to the Heroes Page');
 });
 
-app.get('/heroes', (req, res) =>{
+app.get('/heroes', (req, res) => {
     res.send(heroes);
 });
 
-app.get('/heroes/:id', (req, res) =>{
+app.get('/heroes/:id', (req, res) => {
     let heroId = parseInt(req.params.id);
-    for(let i = 0; i < heroes.length; i++){
-        if(heroes[i].id === heroId){
-            res.send(heroes[i].name);
+    for (let i = 0; i < heroes.length; i++) {
+        if (heroes[i].id === heroId) {
+            res.send(`<h1>${heroes[i].name}</h1>
+                <p>Superpower: ${heroes[i].superPower}</p>
+                <img src=${heroes[i].img} />`
+            );
         };
     };
 });
 
-app.post('/heroes', (req, res) =>{
+app.post('/heroes', (req, res) => {
     let parsedHeroId = parseInt(req.body.id);
     let newHero = {
         id: parsedHeroId,
@@ -58,6 +65,6 @@ app.post('/heroes', (req, res) =>{
 
 
 
-app.listen(PORT, () =>{
+app.listen(PORT, () => {
     console.log(`Server is running on PORT: ${PORT}`);
 });
--- ServerTwo.js
+++ ServerTwo.js
@@ -9,22 +9,25 @@ let villains = [
     {
         id: 0,
         name: 'Sydrome',
-        superPower: 'Zero-Point Energy and Flight'
+        superPower: 'Zero-Point Energy and Flight',
+        img: 'https://vignette.wikia.nocookie.net/disney/images/9/96/Profile_-_Syndrome.png/revision/latest?cb=20190315014302'
     },
     {
-        id: 2,
+        id: 1,
         name: 'Joker',
-        superPower: 'Being the Joker'
+        superPower: 'Being the Joker',
+        img: 'https://cnet1.cbsistatic.com/img/1gzKRz3KNXpEAjFXsecawMw2baI=/980x551/2018/07/16/87518406-dde9-41e5-b99b-8ae54755b4c6/jokers-heathledger-1.jpg'
     },
     {
         id: 2,
         name: 'Thanos',
-        superPower: 'Demi-God'
+        superPower: 'Demi-God',
+        img: 'https://cdn.segmentnext.com/wp-content/uploads/2018/02/avengers_infinity_war_d23_poster.jpg'
     },
 ]
 
 app.get('/', (req, res)=>{
-    res.send('Server is running');
+    res.send('Welcome to the Villains Page');
 });
 
 app.get('/villains', (req, res) =>{
@@ -35,7 +38,10 @@ app.get('/villains/:id', (req, res) =>{
     let villainId = parseInt(req.params.id);
     for(let i = 0; i < villains.length; i++){
         if(villains[i].id === villainId){
-            res.send(villains[i].name);
+            res.send(`<h1>${villains[i].name}</h1>
+                    <p>${villains[i].superPower}</p>
+                    <img src=${villains[i].img} />
+            `);
         };
     };
 });
